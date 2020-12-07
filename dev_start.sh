4555a8d0d08144990bb3c3bd1c150b5448d868cf
n0pj <admin@n0pj.com>
2020-12-07 19:48:06 +0900

    tz

--- dev_start.sh
+++ dev_start.sh
@@ -1,9 +1,9 @@
 # // この順でコマンド実行
 cd web
-git clone https://github.com/shinroh/trust-jv-dealer-system.git
+git clone -b local https://github.com/shinroh/tz-concierge-web-management-version.git
 cd ../
 docker-compose -f docker-compose-develop.yml up -d
-docker exec -it php bash -c '
+docker exec -it ndock_tz_php bash -c '
 composer install &&
 php artisan migrate:fresh &&
 php artisan db:seed &&
