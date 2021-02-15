01bf08d05d7b830c8859084c9754323e4deb46d1
n0pj <admin@n0pj.com>
2021-02-15 10:06:47 +0900

    時刻表示修正

--- ndock_lib/src/time.rs
+++ ndock_lib/src/time.rs
@@ -1,11 +1,11 @@
 use chrono::format::{DelayedFormat, StrftimeItems};
-use chrono::Utc;
+use chrono::{Local, Utc};
 
 pub struct Time {}
 
 impl Time {
   pub fn now(time: Option<&'static str>) -> DelayedFormat<StrftimeItems<'static>> {
-    Utc::now().format(time.unwrap_or("%H:%M:%S"))
+    Local::now().format(time.unwrap_or("%H:%M:%S"))
   }
 
   pub fn to_string(time: DelayedFormat<StrftimeItems<'static>>) -> String {
