e37f8981a60399e7ec403782efba166fdd691972
n0pj <admin@n0pj.com>
2021-02-17 20:01:31 +0900

    rm, rmi のテキスト修正、yaml 修正

---
+++ docker_settings/services/php-fpm/setup.main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# add user
+# cs_*.sh で指定した USER_ID( 引数 ) を受け取る
+USER_ID=$1
+
+# chown
+chown -R ${USER_ID}:${USER_ID} node_modules && chown -R ${USER_ID}:${USER_ID} vendor
+chown -R www:www-data storage/
+chmod -R 774 storage/
+
+# migration
+su -c "composer install" www
+su -c "php artisan migrate:fresh" www
+su -c "php artisan db:seed" www
+su -c "php artisan storage:link" www
+# su -c "yarn" www
